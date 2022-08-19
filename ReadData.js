de775fa96b0ca78a64b29b1e7e98df5993d2aa46
cjproctor0 <98987809+cjproctor0@users.noreply.github.com>
2022-08-19 11:19:10 -0400

    Added different column calls for dynamodb

--- ReadData.js
+++ ReadData.js
@@ -4,12 +4,15 @@ const ddb = new AWS.DynamoDB.DocumentClient({region: 'us-east-1'});
 exports.handler = async (event, context, callback) => {
     await readMessage().then(data => {
         data.Items.forEach(function(item) {
-            console.log(item.firstName)  // match these columns to DynamoDB columns
-            console.log(item.lastName)
-            console.log(item.breedType)
-            console.log(item.sprayedNeutered)
-            console.log(item.rabiesVaccine)
-            console.log(item.bordetellaVaccine)
+            console.log(item.firstname)  // match these columns to DynamoDB columns
+            console.log(item.lastname)
+            console.log(item.breed)
+            console.log(item.spayedneutered)
+            console.log(item.gender)
+            console.log(item.rabiesvaccine)
+            console.log(item.bordetellavaccine)
+            console.log(item.petname)
+            console.log(item.dateofbirth)
         });
         callback(null, {
             statusCode: 200,
@@ -25,8 +28,8 @@ exports.handler = async (event, context, callback) => {
 
 function readMessage() {
     const params = {
-        TableName: 'DogData',
-        //Limit: 30
+        TableName: 'Cloud-Pawssible',
+        //Limit: 3
     }
     return ddb.scan(params).promise();
 }
